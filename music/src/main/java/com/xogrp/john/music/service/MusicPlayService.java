468fafbf3548fd5d3c84d31da312f7423bce4b52
jwu <jwu@xogrp.com>
2017-03-24 19:39:23 +0800

    test

--- music/src/main/java/com/xogrp/john/music/service/MusicPlayService.java
+++ music/src/main/java/com/xogrp/john/music/service/MusicPlayService.java
@@ -3,7 +3,9 @@ package com.xogrp.john.music.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -22,19 +24,38 @@ public class MusicPlayService extends Service {
         }
     }
 
+    private Handler logHandler;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Log.e(TAG, "onCreate: ");
     }
 
+    String test;
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if(intent!= null && intent.hasExtra("test")){
-            Log.e(TAG, "onStartCommand: "+ intent.getStringExtra("test"));
+            test = intent.getStringExtra("test");
+            Log.e(TAG, "onStartCommand: test = "+ test);
+            Log.e(TAG, "onStartCommand: count = "+ count);
         }else{
-            Log.e(TAG, "onStartCommand: ");
+            Log.e(TAG, "onStartCommand: count = "+ count);
         }
+
+        if(logHandler == null){
+            logHandler = new Handler(new Handler.Callback() {
+                @Override
+                public boolean handleMessage(Message msg) {
+                    Log.e(TAG, "MusicPlayService handleMessage: "+msg.what +" test = "+test);
+                    if(!stop) logHandler.sendEmptyMessageDelayed(++count, 3000);
+                    return false;
+                }
+            });
+        }
+
+        logHandler.sendEmptyMessageDelayed(++count, 3000);
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -58,5 +79,9 @@ public class MusicPlayService extends Service {
 
     public int getCount() {
         return count;
+    }
+    boolean stop = false;
+    public void stopLog(){
+        stop = true;
     }
 }
