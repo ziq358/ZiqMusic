463c391f93a7bb1fc82325d8af21b503d5c47268
jwu <jwu@xogrp.com>
2017-04-24 22:53:42 +0800

    排行榜

---
+++ music/src/main/java/com/xogrp/john/music/adapter/RankingAdapter.java
@@ -0,0 +1,123 @@
+package com.xogrp.john.music.adapter;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewCompat;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.recycleviewrelatedlibrary.BaseRecycleViewAdapter;
+import com.example.recycleviewrelatedlibrary.BaseRecycleViewViewHolder;
+import com.example.recycleviewrelatedlibrary.BaseRecycleViewViewType;
+import com.xogrp.john.music.R;
+import com.xogrp.john.music.fragment.home.discover.MVP_Song_List.SongListItemModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by john on 14/04/2017.
+ */
+
+public class RankingAdapter extends BaseRecycleViewAdapter<String> {
+
+    public final static int INT_ONE_ITEM_VIEW_TYPE = 0;
+    private final static int INT_TWO_ITEM_VIEW_TYPE = 1;
+    public final static int INT_LOAD_MORE_VIEW_TYPE = 2;
+
+    private List<String> mDataList = new ArrayList<>();
+    public RankingAdapter(Context context) {
+        super(context);
+        mDataList.add("1");
+        mDataList.add("2");
+        mDataList.add("3");
+        setData(mDataList);
+    }
+
+    @Override
+    protected void initViewType(List<BaseRecycleViewViewType> viewTypesList) {
+        viewTypesList.add(new OneViewType(this));
+        viewTypesList.add(new TwoViewType(this));
+        viewTypesList.add(new ThreeViewType(this));
+    }
+
+
+    private class OneViewType extends BaseRecycleViewViewType{
+
+        public OneViewType(BaseRecycleViewAdapter adapter) {
+            super(adapter);
+        }
+
+        @Override
+        protected boolean isMatchViewType(int position) {
+            return position == getItemViewType();
+        }
+
+        @Override
+        public int getItemViewType() {
+            return INT_ONE_ITEM_VIEW_TYPE;
+        }
+
+        @Override
+        protected int getLayoutRes() {
+            return R.layout.ranking_one_item_layout;
+        }
+
+        @Override
+        protected void onBindViewHolder(BaseRecycleViewViewHolder holder, int position) {
+        }
+    }
+
+    private class TwoViewType extends BaseRecycleViewViewType{
+
+        public TwoViewType(BaseRecycleViewAdapter adapter) {
+            super(adapter);
+        }
+
+        @Override
+        protected boolean isMatchViewType(int position) {
+            return position == getItemViewType();
+        }
+
+        @Override
+        public int getItemViewType() {
+            return INT_TWO_ITEM_VIEW_TYPE;
+        }
+
+        @Override
+        protected int getLayoutRes() {
+            return R.layout.ranking_two_item_layout;
+        }
+
+        @Override
+        protected void onBindViewHolder(BaseRecycleViewViewHolder holder, int position) {
+        }
+    }
+
+    private class ThreeViewType extends BaseRecycleViewViewType{
+
+        public ThreeViewType(BaseRecycleViewAdapter adapter) {
+            super(adapter);
+        }
+
+        @Override
+        protected boolean isMatchViewType(int position) {
+            return position == getItemViewType();
+        }
+
+        @Override
+        public int getItemViewType() {
+            return INT_LOAD_MORE_VIEW_TYPE;
+        }
+
+        @Override
+        protected int getLayoutRes() {
+            return R.layout.ranking_three_item_layout;
+        }
+
+        @Override
+        protected void onBindViewHolder(BaseRecycleViewViewHolder holder, int position) {
+        }
+    }
+
+}
--- music/src/main/java/com/xogrp/john/music/fragment/home/discover/RankingListFragment.java
+++ music/src/main/java/com/xogrp/john/music/fragment/home/discover/RankingListFragment.java
@@ -2,12 +2,19 @@ package com.xogrp.john.music.fragment.home.discover;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.xogrp.john.music.R;
+import com.xogrp.john.music.adapter.RankingAdapter;
+import com.xogrp.john.music.adapter.RecommendAdapter;
 import com.xogrp.john.music.fragment.BaseMusicFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by john on 16/03/2017.
@@ -16,6 +23,9 @@ import com.xogrp.john.music.fragment.BaseMusicFragment;
 public class RankingListFragment extends BaseMusicFragment {
 
     public static final String FRAGMENT_TAG = "ranking_list_fragment";
+
+    @BindView(R.id.rv_ranking)
+    RecyclerView mRecyclerView;
 
     @Override
     public String getFragmentTag() {
@@ -28,6 +38,14 @@ public class RankingListFragment extends BaseMusicFragment {
         return inflater.inflate(R.layout.fragment_ranking_list, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+        RankingAdapter adapter = new RankingAdapter(getContext());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(adapter);
+    }
 
 
 }
